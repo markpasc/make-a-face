3eaeb40f1c76808e5315d05efe19693c7487a284
Mark Paschal <markpasc@markpasc.org>
2010-01-19 15:22:56 -0800

    Redirect permalink pages to the home page for now

--- makeaface/makeaface/urls.py
+++ makeaface/makeaface/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(r'^$', 'makeaface.views.home', name='home'),
     url(r'^$', 'makeaface.views.home', name='group_events'),
+    url(r'^entry/(?P<id>\w+)$', 'django.views.generic.simple.redirect_to',
+        {'url': r'/#%(id)s'}),
     url(r'^upload$', 'makeaface.views.upload_photo', name='upload_photo'),
     url(r'^favorite$', 'makeaface.views.favorite', name='favorite'),
     url(r'^flag$', 'makeaface.views.flag', name='flag'),
