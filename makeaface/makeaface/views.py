e2fb464642d5b7c131ebc4faf5f8838dc344cb51
Mark Paschal <markpasc@markpasc.org>
2010-01-25 14:15:53 -0800

    Don't mess with user.xid if there's no user to mess with

--- makeaface/makeaface/views.py
+++ makeaface/makeaface/views.py
@@ -84,16 +84,18 @@ def photo_for(request):
 
 
 def photo(request, xid):
-    user_xid = request.user.xid
     with typepad.client.batch_request():
         photo = Asset.get_by_url_id(xid)
         favs = photo.favorites
 
-    try:
-        with typepad.client.batch_request():
-            userfav = Favorite.get_by_user_asset(user_xid, xid)
-    except Favorite.NotFound:
-        userfav = None
+    userfav = None
+    if request.user.is_authenticated():
+        user_xid = request.user.xid
+        try:
+            with typepad.client.batch_request():
+                userfav = Favorite.get_by_user_asset(user_xid, xid)
+        except Favorite.NotFound:
+            pass
 
     return TemplateResponse(request, 'makeaface/photo.html', {
         'photo': photo,
