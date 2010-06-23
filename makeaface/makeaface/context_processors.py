f3126c84b5a314a9249215d78f375f691037e267
Mark Paschal <markpasc@markpasc.org>
2010-06-23 14:59:59 -0400

    Have a mobile home page

--- makeaface/makeaface/context_processors.py
+++ makeaface/makeaface/context_processors.py
@@ -10,3 +10,22 @@ def ganalytics(request):
         return {
             'ganalytics_code': code,
         }
+
+
+def mobile(request):
+    """
+    Identifies whether the user agent string is for a 'mobile' class
+    device and assigns a 'mobile' context variable appropriately.
+
+    """
+    if request.GET.get('mobile', None):
+        return {'mobile': True}
+
+    agent = request.META['HTTP_USER_AGENT']
+    mobile = False
+    if (('AppleWebKit' in agent) and ('Mobile' in agent or 'Pre' in agent or 'webOS' in agent)) or ('Opera Mini' in agent):
+        if 'iPad' not in agent: # but not the iPad
+            mobile = True
+    return {
+        'mobile': mobile
+    }
